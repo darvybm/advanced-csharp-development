e80004c0d4d4f8b4c692087bbee214a7ece014ec
darvybm <darvybetance@gmail.com>
2025-05-09 18:09:55 -0400

    refactor: movido fuera de carpeta redundante

---
+++ TaskManagementAPI/Config/ErrorHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using TaskManagementAPI.DTOs;
+
+namespace TaskManagementAPI.Config
+{
+    public class ErrorHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var response = TaskResponse<object>.Fail(
+                    "Ocurrió un error inesperado.",
+                    error.Message
+                );
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        }
+
+    }
+}
